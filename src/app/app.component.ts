8f00901f3b57f2a2073346899572920006974c2a
StephZcj <stephzcj@aliyun.com>
2017-06-07 17:38:01 +0800

    整合bootstrap首页初探

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component ,Input,trigger,state,style,animate,transition,keyframes} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('flyIn', [
+      transition('void => *', [   
+        animate('0.5s ease-out', keyframes([ // 回弹的效果
+          style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+        ]))
+      ])
+    ])
+   ]
 })
 export class AppComponent {
-  title = 'app works!';
+  
 }
