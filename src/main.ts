db784e76a127f4a61317f29ae2f3d443bbedb82f
StephZcj <stephzcj@aliyun.com>
2017-06-22 17:34:25 +0800

    小修正

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,11 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+ platformBrowserDynamic().bootstrapModule(AppModule);
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+
+
+
+
